820f0ae376fa6d7a90a763ee09c03a2d3e3b93e6
onesider <onesider@gmail.com>
2017-03-28 16:43:59 +0900

    study

---
+++ urlparse.py
@@ -0,0 +1,27 @@
+#URL 파싱..
+from urllib import parse
+
+result = parse.urlparse('https://www.python.org/doc/;parameter?q=example#hoge')
+
+print('url')
+print(result)
+print(result.geturl())
+print(result.scheme)
+print(result[0:3])
+print(result.hostname)
+
+print('\nquery')
+result2 = parse.urlparse('https://google.co.kr/search?q=python&oq=python&sourceid=chrome&ie=UTF-8')
+print(result2.query)
+
+#딕셔너리형태로 출력
+print(parse.parse_qs(result2.query))
+
+#하나의 키에 값이 두개 일때
+print(parse.parse_qs('key=1&key=2'))
+
+#파싱된 결과를 퓨플로 받고싶을때
+print(parse.parse_qsl(result2.query))
+#
+#키값이 같을때 두개다 출력
+print(parse.parse_qsl('key=1&key=2'))
