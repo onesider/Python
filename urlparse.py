2292c05e5f7733fd7a28776a078d99bba17bf362
onesider <onesider@gmail.com>
2017-03-29 14:47:26 +0900

    20170329

--- urlparse.py
+++ urlparse.py
@@ -22,6 +22,11 @@ print(parse.parse_qs('key=1&key=2'))
 
 #파싱된 결과를 퓨플로 받고싶을때
 print(parse.parse_qsl(result2.query))
-#
+
 #키값이 같을때 두개다 출력
 print(parse.parse_qsl('key=1&key=2'))
+
+#keep_blank_values 옵션 차이점
+print("")
+print(parse.parse_qs('key1=&key2=hong'))
+print(parse.parse_qs('key1=&key2=hong', keep_blank_values=True))
